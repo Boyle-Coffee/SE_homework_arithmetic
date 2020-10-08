aa53a50e0a771b912ed363d35107c579cff83602
YaYaHuang <570911275@qq.com>
2020-10-08 16:13:43 +0800

    重构：项目基本架构

---
+++ 项目代码/src/com/company/service/impl/DemoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.company.service.impl;
+
+import com.company.dao.Demo;
+import com.company.service.DemoService;
+
+/**
+ * @program: 项目代码
+ * @description: 业务层接口实现类Demo
+ * @author: Mr.Huang
+ * @create: 2020-10-08 16:10
+ **/
+public class DemoServiceImpl implements DemoService {
+
+    Demo demo = new Demo();
+
+    @Override
+    public void demo() {
+        System.out.println("调用具体实现方法");
+        demo.demo();
+    }
+}
