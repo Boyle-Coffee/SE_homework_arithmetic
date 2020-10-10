411ebcf3b605bcc48c461fbc04b1b2c376acfc34
Boyle-Coffee <2018770887@qq.com>
2020-10-10 21:46:01 +0800

    修复：修复了加法运算的问题，删除本地项目配置文件

--- 项目代码/src/com/company/test/CalculateRPNTest.java
+++ 项目代码/src/com/company/test/CalculateRPNTest.java
@@ -13,7 +13,7 @@ public class CalculateRPNTest {
     private static CalculateMdlService test = new CalculateMdlServiceImpl();
 
     public static void main(String[] args) {
-        String exper = "( 8'6/7 - 10 ÷ 0 )";
+        String exper = "68 + 61";
         for(int i=0; i <=10; i++) {
             String res = test.generateReversePoland(exper);
             System.out.println(i);
