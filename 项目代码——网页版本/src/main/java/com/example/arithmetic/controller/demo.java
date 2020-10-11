487515440ff004faf4e820e0a85b34d1e6a64330
YaYaHuang <570911275@qq.com>
2020-10-11 16:36:08 +0800

    功能：网页版四则运算架构

---
+++ 项目代码——网页版本/src/main/java/com/example/arithmetic/controller/demo.java
@@ -0,0 +1,13 @@
+package com.example.arithmetic.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @program: arithmetic
+ * @description:
+ * @author: Mr.Huang
+ * @create: 2020-10-11 16:07
+ **/
+@Controller
+public class demo {
+}
